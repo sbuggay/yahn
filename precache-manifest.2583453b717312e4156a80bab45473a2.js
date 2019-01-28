74ea51976bf473de42ce4201be8eb4bca6142327
Devan Buggay <sbuggay@gmail.com>
2019-01-27 22:43:26 -0800

    Updates

---
+++ precache-manifest.2583453b717312e4156a80bab45473a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "58f046ebb448b9486365",
+    "url": "/yahn/static/js/runtime~main.58f046eb.js"
+  },
+  {
+    "revision": "007cadb0cd493e26df9a",
+    "url": "/yahn/static/js/main.007cadb0.chunk.js"
+  },
+  {
+    "revision": "2e520cbbd09c33cb7291",
+    "url": "/yahn/static/js/1.2e520cbb.chunk.js"
+  },
+  {
+    "revision": "007cadb0cd493e26df9a",
+    "url": "/yahn/static/css/main.c8296efa.chunk.css"
+  },
+  {
+    "revision": "d5d943595a28304be10393fd9527a6b5",
+    "url": "/yahn/index.html"
+  }
+];
